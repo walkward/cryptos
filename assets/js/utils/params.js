6998a991ae2cab64712dde7397fa239fe34f5384
Walker Ward <walker.ward1@gmail.com>
2017-12-30 17:28:47 -0700

    deploy

--- assets/js/utils/params.js
+++ assets/js/utils/params.js
@@ -1,1 +1,2 @@
-module.exports = JSON.parse('{"' + decodeURI(location.search.substring(1).replace(/&/g, '","').replace(/=/g, '":"')) + '"}')
+let params = location.search !== '' ? JSON.parse('{"' + decodeURI(location.search.substring(1).replace(/&/g, '","').replace(/=/g, '":"')) + '"}') : {}
+module.exports = params
